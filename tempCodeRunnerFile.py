e638cb3c47e8b90e07a2727209b99a475da4f20c
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-01 13:14:08 -0400

    Day 1 project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator!")
+town_name = input("What is the name of the town you grew up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name could be " + town_name + " " + pet_name)
