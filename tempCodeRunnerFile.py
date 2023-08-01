fccf4f5c47d18fe2bc506161ce1751a55eb86c81
Chamath Jayarathne <38222142+notchamath@users.noreply.github.com>
2023-08-01 14:22:21 -0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to band name generator!")
-town_name = input("What is the name of the town you grew up in?\n")
-pet_name = input("What is the name of your first pet?\n")
-print("Your band name could be " + town_name + " " + pet_name)
