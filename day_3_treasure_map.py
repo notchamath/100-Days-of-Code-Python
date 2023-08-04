1636eac8dd88ebcd456f91efb6befd44e0567255
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-03 20:01:11 -0400

    Day 3: Treasure game

---
+++ day_3_treasure_map.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+q = input("You are at a crossroads. Do you want to go left or right? ").lower()
+
+if(q == 'left'):
+
+  q = input("You are at a river, a ferry will be there in 1 hour. Do you want to go swim or wait? ").lower()
+  
+  if(q == 'wait'):
+    q = input("You see 3 door. Which do you open? Red, yellow or green? ").lower()
+    
+    if(q == 'yellow'):
+      print("You Found the Treasure! Well done!")
+    else:
+      print("Game over! You got taken out by killer ninjas.")
+  
+  else:
+    print("Game over! You got eaten by a Croc.")
+   
+else:
+    print("Game over! You fell into a hole.")
+  
