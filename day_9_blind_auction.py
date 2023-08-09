fb973fac361a6e240376d0ecd7b6b8e5eefda8f9
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-09 16:09:17 -0400

    fix: remove for loop

--- day_9_blind_auction.py
+++ day_9_blind_auction.py
@@ -1,6 +1,8 @@
 import os 
 
 bids = {}
+winner = None
+largest = 0
     
 def clear(): os.system("clear")
 
@@ -10,6 +12,10 @@ while True:
 
     bids[name] = bid
 
+    if bids[name] > largest:
+        largest = bids[name]
+        winner = name
+    
     restart = input("Type \"yes\" if there are other users who want to place bids: ").lower()
 
     if restart != "yes":
@@ -17,12 +23,4 @@ while True:
 
     clear()
 
-winner = None
-largest = 0
-for name in bids:
-    if bids[name] > largest:
-        largest = bids[name]
-        winner = name
-
 print(f"{winner} won the blind auction with a bid of ${'{0:.2f}'.format(largest)}!")
-print(bids)
