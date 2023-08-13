9e3ba58252848f48d46e57f86fcd367ba216ab0e
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-13 19:09:45 -0400

    fix: Modularize code more

--- day_12_num_guessing_game.py
+++ day_12_num_guessing_game.py
@@ -1,29 +1,40 @@
 import random
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-
-# Get difficulty from user
-while True:
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-    if difficulty == "easy" or difficulty == "hard":
-        break
-
-# Attempts left
-attempts = 10
-if difficulty == "hard": attempts = 5
+# Difficulty Levels
+ATTEMPTS_EASY = 10
+ATTEMPTS_HARD = 5
 
 # Random number
 NUM = random.randint(1, 100)
 
-def play(attempts):
+# Welcome message
+def welcome_msg():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+# Get difficulty from user
+def get_difficulty():
+    
+    while True:
+        difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+        if difficulty == "easy" or difficulty == "hard":
+            break
+    
+    if difficulty == "easy": return ATTEMPTS_EASY
+    return ATTEMPTS_HARD
+
+# Start game
+def play():
+    welcome_msg()
+
+    attempts = get_difficulty()
     
     is_game_over = False
 
     while not is_game_over:
 
         if attempts > 0:
-            print(f"You have {attempts} left.")
+            print(f"You have {attempts} guesses left.")
             guess = int(input("Make a guess: "))
             attempts -= 1
             
@@ -39,4 +50,4 @@ def play(attempts):
             print(f"The correct number was {NUM}.")
             is_game_over = True
 
-play(attempts)
+play()
